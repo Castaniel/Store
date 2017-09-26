420990e1869364789f6e8644289baf5ea158057f
Marek <yurchylyak@gmail.com>
2017-09-26 12:17:03 +0000

    struggle with my too damn low IQ

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @counter = session[:counter]
+    @counter.nil? ? @counter = 1 : @counter+= 1
+    session[:counter] = @counter
+    if session[:counter] > 5
+      return session[:counter]
+    end
   end
 end
